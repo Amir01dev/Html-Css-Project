1e20479fe55e866ca30138a98172af2da95040a2
Amirreza Jamshidi <amir010101developer@gmail.com>
2023-10-11 09:27:43 +0330

    Update header style

--- personal-website/scripts/app.js
+++ personal-website/scripts/app.js
@@ -1,5 +1,7 @@
 const navToggleIcon = document.querySelector('.menu__toggle-icon')
+const menu = document.querySelector('.menu')
 
 navToggleIcon.addEventListener('click', function () {
     this.classList.toggle('menu__toggle-icon--open')
+    menu.classList.toggle('menu--open')
 })
