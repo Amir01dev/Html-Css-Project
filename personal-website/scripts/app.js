34697cf66a3c162c656116e36f5d3193c0bc5c66
Amirreza Jamshidi <amir010101developer@gmail.com>
2023-10-11 11:23:38 +0330

    coding header responsive

--- personal-website/scripts/app.js
+++ personal-website/scripts/app.js
@@ -1,7 +1,9 @@
 const navToggleIcon = document.querySelector('.menu__toggle-icon')
 const menu = document.querySelector('.menu')
+const cover = document.querySelector('.cover')
 
 navToggleIcon.addEventListener('click', function () {
     this.classList.toggle('menu__toggle-icon--open')
     menu.classList.toggle('menu--open')
+    cover.classList.toggle('cover--show')
 })
