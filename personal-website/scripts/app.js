e6db7e4c3593e5ccf7f97f6112f0523a6228fc90
Amirreza Jamshidi <amir010101developer@gmail.com>
2023-10-13 16:27:19 +0330

    add about us stucture

--- personal-website/scripts/app.js
+++ personal-website/scripts/app.js
@@ -1,9 +1,8 @@
-const navToggleIcon = document.querySelector('.menu__toggle-icon')
+const navToggleIcon = document.querySelector('.nav__toggle-icon')
 const menu = document.querySelector('.menu')
 const cover = document.querySelector('.cover')
-
-navToggleIcon.addEventListener('click', function () {
-    this.classList.toggle('menu__toggle-icon--open')
+navToggleIcon.addEventListener('click',function (){
+    this.classList.toggle('nav__toggle-icon--open')
     menu.classList.toggle('menu--open')
     cover.classList.toggle('cover--show')
 })
